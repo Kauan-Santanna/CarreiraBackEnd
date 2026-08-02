8199671e166ea87e92e883ced722ab75fbc45c02
Kauan-Santanna <1kn.santanna@gmail.com>
2026-08-02 10:05:48 -0300

    Boas praticas

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -3,4 +3,7 @@ print("Sabor Express\n")
 print("1. Cadastrar restaurante")
 print("1. Listar restaurante")
 print("1. Ativar restaurante")
-print("1. Sair")
+print("1. Sair\n")
+
+opcao_escolhida = input("Escolha uma opção: ")
+print(f"Você escolheu a opçao {opcao_escolhida}")
