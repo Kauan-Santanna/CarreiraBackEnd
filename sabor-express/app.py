6568d36ee4d9682e60ca712bab9334944af79a90
Kauan-Santanna <1kn.santanna@gmail.com>
2026-08-03 20:54:45 -0300

    Aprendendo sobre if else

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -8,9 +8,20 @@ print("""
 """)
 
 print("1. Cadastrar restaurante")
-print("1. Listar restaurante")
-print("1. Ativar restaurante")
-print("1. Sair\n")
+print("2. Listar restaurantes")
+print("3. Ativar restaurante")
+print("4. Sair\n")
 
 opcao_escolhida = input("Escolha uma opção: ")
 print(f"Você escolheu a opçao {opcao_escolhida}")
+
+if opcao_escolhida == "1":
+    print("Cadastrar restaurante")
+elif opcao_escolhida == "2":
+    print("Listar restaurantes")
+elif opcao_escolhida == "3":
+    print("Ativar restaurante")
+elif opcao_escolhida == "4":
+    print("Encerrando o programa")
+else:
+    print("Alternativa incorreta!")
