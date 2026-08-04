006a65b2d75f54b0bcb04ff7a9f1028d05b2c872
Kauan-Santanna <1kn.santanna@gmail.com>
2026-08-03 21:17:12 -0300

    Funções e import

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,3 +1,5 @@
+import os
+
 print("""
  ██████╗ █████╗ ██████╗  █████╗ ██████╗   ███████╗██╗  ██╗██████╗ ██████╗ ███████╗ ██████╗ ██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -12,16 +14,17 @@ print("2. Listar restaurantes")
 print("3. Ativar restaurante")
 print("4. Sair\n")
 
-opcao_escolhida = input("Escolha uma opção: ")
-print(f"Você escolheu a opçao {opcao_escolhida}")
+opcao_escolhida = int(input("Escolha uma opção: "))
 
-if opcao_escolhida == "1":
+def finalizar_app():
+    os.system("cls")
+    print("Finalizando o app\n")
+
+if opcao_escolhida == 1:
     print("Cadastrar restaurante")
-elif opcao_escolhida == "2":
+elif opcao_escolhida == 2:
     print("Listar restaurantes")
-elif opcao_escolhida == "3":
+elif opcao_escolhida == 3:
     print("Ativar restaurante")
-elif opcao_escolhida == "4":
-    print("Encerrando o programa")
 else:
-    print("Alternativa incorreta!")
+    finalizar_app()
