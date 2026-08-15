6a2b601b931de4516bdf4a561df33b78071d8603
Kauan-Santanna <1kn.santanna@gmail.com>
2026-08-15 18:14:55 -0300

    Refatorando código

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -3,6 +3,56 @@ from termcolor import colored
 
 restaurantes = ["Pizza", "Hamburguer"]
 
+# ==============================
+# FUNÇÕES AUXILIARES
+# ==============================
+
+def voltar_ao_menu_principal():
+    input(colored("Pressione ENTER para voltar ao menu principal... ", "yellow"))
+    main()
+
+def limpar_tela():
+    os.system("cls")
+
+# ==============================
+# FUNÇÕES DO SISTEMA
+# ==============================
+
+def cadastrar_novo_restaurante():
+    limpar_tela()
+    print(colored("=-" * 15, "white"))
+    print(colored("Cadastro de novos restaurantes", "yellow"))
+    print(colored("=-" * 15, "white"))
+
+    nome_do_restaurante = input(colored("\n• Digite o nome do restaurante que deseja cadastrar: ", "yellow"))
+    restaurantes.append(nome_do_restaurante)
+    print(colored(f"• O restaurante ", "yellow") + colored(nome_do_restaurante, "green") + colored(" foi cadastrado com sucesso!\n", "yellow"))
+
+    print(colored("=-" * 24, "white"))
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    limpar_tela()
+    print(colored("=-" * 15, "white"))
+    print(colored(f"{"Listando restaurantes":^30}", "yellow"))
+    print(colored("=-" * 15, "white"))
+    print("")
+
+    for restaurante in restaurantes:
+        print(colored("• ", "white") + colored(restaurante, "yellow"))
+
+    print("")
+    print(colored("=-" * 24, "white"))
+    voltar_ao_menu_principal()
+
+def finalizar_app():
+    limpar_tela()
+    print(colored("Finalizando o app\n", "green"))
+
+# ==============================
+# MENU
+# ==============================
+
 def exibir_nome_do_programa():
     print(colored("""
  ██████╗ █████╗ ██████╗  █████╗ ██████╗   ███████╗██╗  ██╗██████╗ ██████╗ ███████╗ ██████╗ ██████╗
@@ -18,44 +68,6 @@ def exibir_opcoes():
     print(colored("2. ", "white") + colored("Listar restaurantes", "yellow"))
     print(colored("3. ", "white") + colored("Ativar restaurante", "yellow"))
     print(colored("4. ", "white") + colored("Sair\n", "yellow"))
-
-def finalizar_app():
-    os.system("cls")
-    print(colored("Finalizando o app\n", "green"))
-
-def opcao_invalida():
-    print(colored("Opção inválida!\n", "red"))
-    input(colored("Digite uma tecla para voltar ao menu principal: ", "cyan"))
-    main()
-
-def cadastrar_novo_restaurante():
-    os.system("cls")
-    print(colored("=-" * 15, "white"))
-    print(colored("Cadastro de novos restaurantes", "yellow"))
-    print(colored("=-" * 15, "white"))
-
-    nome_do_restaurante = input(colored("\n• Digite o nome do restaurante que deseja cadastrar: ", "yellow"))
-    restaurantes.append(nome_do_restaurante)
-    print(colored(f"• O restaurante ", "yellow") + colored(nome_do_restaurante, "green") + colored(" foi cadastrado com sucesso!\n", "yellow"))
-
-    print(colored("=-" * 24, "white"))
-    input(colored("Pressione ENTER para voltar ao menu principal... ", "yellow"))
-    main()
-
-def listar_restaurantes():
-    os.system("cls")
-    print(colored("=-" * 15, "white"))
-    print(colored(f"{"Listando restaurantes":^30}", "yellow"))
-    print(colored("=-" * 15, "white"))
-    print("")
-
-    for restaurante in restaurantes:
-        print(colored("• ", "white") + colored(restaurante, "yellow"))
-
-    print("")
-    print(colored("=-" * 24, "white"))
-    input(colored("Pressione ENTER para voltar ao menu principal... ", "yellow"))
-    main()
 
 def escolher_opcao():
     try:
@@ -74,8 +86,16 @@ def escolher_opcao():
     except:
         opcao_invalida()
 
+def opcao_invalida():
+    print(colored("Opção inválida!\n", "red"))
+    voltar_ao_menu_principal()
+
+# ==============================
+# PROGRAMA
+# ==============================
+
 def main():
-    os.system("cls")
+    limpar_tela()
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
